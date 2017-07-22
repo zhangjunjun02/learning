3105b507da91cf6a70b7484c0ff8c74da02dbcb6
zhangju02njun <956572126@qq.com>
2017-07-22 12:27:31 +0800

    test

---
+++ src/main/java/com/gtcy/SpringBootDomeApplication.java
@@ -0,0 +1,14 @@
+package com.gtcy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDomeApplication {
+
+	
+	    
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDomeApplication.class, args);
+	}
+}
---
+++ src/test/java/com/gtcy/SpringBootDomeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.gtcy;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootDomeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
